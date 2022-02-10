0383b2b2d249229546a83d2c378e396a753a3811
Rix565 <61545240+Rix565@users.noreply.github.com>
2022-02-10 13:22:15 +0100

    fixed a typo that would cause the library to crash

--- 3ds_base.php
+++ 3ds_base.php
@@ -24,7 +24,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-function init3DS($localtionURI){
+function init3DS($locationURI){
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         header('Ugm-SessionID: test');
         header('Location: '. $locationURI);
